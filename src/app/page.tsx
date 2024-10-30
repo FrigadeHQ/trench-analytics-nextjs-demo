feb3d89c9c28fb7b5eaba585087a47056113df7c
christianmat <christian_mathiesen@alumni.brown.edu>
2024-10-30 13:11:13 -0700

    fix: pixel pushing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import { getEventsFromTrench } from "@/data/trench"
-import { cx } from "@/lib/utils"
 import dynamic from "next/dynamic"
 import Header from "./_components/Header"
 import { ReferrersTable } from "./_components/ReferrersTable"
@@ -23,19 +22,14 @@ export default async function Page() {
     <div className="mx-auto max-w-screen-xl">
       <header className="flex justify-end p-4"></header>
 
-      <main
-        className={cx(
-          "lg:pl-0",
-          "ease transform-gpu transition-all duration-100 will-change-transform lg:bg-gray-50 lg:py-3 lg:pr-3 lg:dark:bg-gray-950",
-        )}
-      >
+      <main className="ease transform-gpu transition-all duration-100 will-change-transform lg:bg-gray-50 lg:py-3 lg:pl-3 lg:pr-3 lg:dark:bg-gray-950">
         <div className="bg-white p-4 sm:p-6 lg:rounded-lg lg:border lg:border-gray-200 dark:bg-gray-925 lg:dark:border-gray-900">
           <Header />
           <section className="my-8">
             <div className="space-y-12">
               <VisitorsChart data={visitorsData} />
             </div>
-            <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+            <div className="mt-4 grid grid-cols-1 gap-4 md:grid-cols-2">
               <div>
                 <h2 className="text-lg font-semibold text-gray-900 dark:text-gray-50">
                   Top Pages
